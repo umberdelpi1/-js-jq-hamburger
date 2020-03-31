16e9b5796b66f22c28b55288317bc86fb4da29da
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-31 18:03:27 +0200

    Ex finito

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 
 // primo click, appare il menù;
 
-$('.hamburger-menu').fadeOut();
+//$('.hamburger-menu').fadeOut();
 
 var statoHamburgerMenu = 0;
 
-$ ('ul').click(
+$ ('.fas.fa-bars').click(
   function(){
-    if(statoHamburgerMenu === 0) {
+    if(statoHamburgerMenu == 0) {
       $('.hamburger-menu').fadeIn(2000);
       statoHamburgerMenu = 1;
     }else{
       $('.hamburger-menu').fadeOut(2000);
       statoHamburgerMenu = 0;
     }
+  }
+);
+
+$(".close").click(
+  function (){
+    $('.hamburger-menu').fadeOut(2000);
   }
 );
 
